1daeb4a1ac071a2a8f06b21bf45f57eaf37795bf
= <=>
2022-07-28 17:23:02 +0600

    commit + bootstrap + regis

--- shop/main/views.py
+++ shop/main/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    usernames = ["Erlan", "Arman", "Usman"]
-    return render(request, 'main/index.html', {"names": usernames})
+    return render(request, 'main/index.html', {})
